d71f1379103714d3daea4389d4766a9349e595ea
Jasmitha1474 <jasmithapnair@gmail.com>
2026-03-02 10:25:52 +0530

    [Jasmitha] Added UC2 - HardCoded pallindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,16 +1,25 @@
 public class PalindromeCheckerApp {
     /**
-     * Application entry point.
-     *
-     * This is the first method executed by the JVM
-     * when program starts.
+     * Application entry point FOR uc2.
      *
      * @param args Command-line arguements
      */
     public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System Intialised successfully");
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
 
     }
 }
